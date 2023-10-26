37f056146410300c1a2e0d616ff747b167b02b06
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-10-26 20:21:56 +0530

    update the redux store for 2 hours

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import store from '../store/store'; 
+import {store,persistor} from '../store/store'; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </SessionProvider>
   );
--- store/store.js
+++ store/store.js
@@ -1,4 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import userReducer from "./userReducer";
 import thunk from "redux-thunk";
 
@@ -6,6 +8,28 @@ const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+  // Optionally, add a timeout for the persisted state
+  // This will expire the state after 2 hours (7200000 milliseconds)
+  stateReconciler: (inboundState, originalState, reducedState) => {
+    const persistedState = { ...reducedState, ...inboundState };
+    const now = new Date().getTime();
+    const lastUpdated = persistedState._lastUpdated || now;
+    const isExpired = now - lastUpdated > 7200000;
 
-export default store;
+    if (isExpired) {
+      return originalState;
+    }
+
+    return { ...persistedState, _lastUpdated: now };
+  },
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+
+export { store, persistor };
