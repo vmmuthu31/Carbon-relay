39174a6f8150e9d012e1755666101c13abfb6cf5
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-11-13 21:08:59 +0530

    updated the bid section

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1231,7 +1231,7 @@ const copyToClipboard = () => {
                     <span className='my-2 text-center flex text-xl justify-center text-black'>Incoming Bids</span>
                 </button>
             </div>
-            <button onClick={closeModal1} className="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
+            <button onClick={()=>{closeModal1(); setSelectedProjectId("")}} className="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
                 <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 11-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z" clipRule="evenodd"/>
                 </svg>
