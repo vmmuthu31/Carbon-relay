47bde63ba21bcaa30503cfcde293f26f3b58d970
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-11-21 19:32:29 +0530

    revamping the landing page

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -6,18 +6,6 @@ import { getProviders, signIn, signOut, useSession} from "next-auth/react";
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 
-interface NavigationItem {
-  name: string;
-  href: string;
-  current: boolean;
-}
-
-const navigation = [
-  { name: 'Home', href: "/Dashboard", current: true },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Offers', href: '#', current: false },
-  { name: 'Bids', href: '#', current: false },
-]
 
 
 function classNames(...classes: (string | undefined | null | false)[]): string {
@@ -76,21 +64,7 @@ function Navbar() {
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             
               <div className="hidden sm:ml-6 sm:block pr-2">
-                <div className="flex space-x-4">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'rounded-md px-3 py-2 text-sm font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
+                
               </div>
 
               {/* Profile dropdown */}
@@ -156,20 +130,7 @@ function Navbar() {
 
         <Disclosure.Panel className="sm:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            {navigation.map((item) => (
-              <Disclosure.Button
-                key={item.name}
-                as="a"
-                href={item.href}
-                className={classNames(
-                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium'
-                )}
-                aria-current={item.current ? 'page' : undefined}
-              >
-                {item.name}
-              </Disclosure.Button>
-            ))}
+          
           </div>
         </Disclosure.Panel>
       </>
