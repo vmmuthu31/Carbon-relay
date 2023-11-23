977e93d662a633793dac48f0f598fc26df56550f
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-11-23 09:32:38 +0530

    updated the hero bg images

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import {useSession} from "next-auth/react";
+import { CiPlay1 } from "react-icons/ci";
 
 const Home: React.FC = () => {
   const { data: session } = useSession();
@@ -15,36 +16,37 @@ const Home: React.FC = () => {
   },[session])
   console.log(session)
   return (
-    <div className="min-h-full bg-white">
+    <div className=" bg-[url('../assets/hero.png')]  bgdm  bg-no-repeat  ">
       <Navbar />
       <main className="">
-      <div className="relative isolate px-6 lg:px-8">
+      <div className="relative  isolate ">
         
-        <div className="mx-auto max-w-2xl  lg:pt-28">
-        \
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Lorem ipsum  dolor sit  amet consectetur.
+        <div className=" bg-[url('../assets/hero2.png')] bg-contain bg-no-repeat bgdm2 py-40   lg:pt-28">
+        
+          <div className="text-center  pb-52">
+            <h1 className="text-4xl  tex-center font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Unlock a New Era of <br/> Business Partnerships
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-            Describe exactly what your product or service does to solve this problem. Avoid using verbose words or phrases.
-            </p>
+            Welcome to Carbon Relay, Where Transparency Defines Transactions            </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="/Login"
-                className="rounded-md bg-btn-landing px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md flex bg-btn-landing px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Launch Carbon Relay
+                Launch Carbon Relay <CiPlay1 className=" mx-2 text-lg mt-1 " />
+
               </Link>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+            
             </div>
           </div>
         </div>
       
       </div>
       </main>
+      <div className="bg-[url('../assets/hero1.png')] py-20 bg-no-repeat bg-contain bgdm">
+
+      </div>
     </div>
   );
 };
